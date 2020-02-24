2cb1c7afe43a861d385a1877d590df42f8d217b1
Ben Fung <benfung1996@gmail.com>
2020-02-23 22:53:13 -0800

    updated DOM and jquery
    added jquery functions and var. adjusted HTML DOM.

--- TriviaGame/assets/javascript/app.js
+++ TriviaGame/assets/javascript/app.js
@@ -6,32 +6,96 @@ $(document).ready(function() {
 
     var correct = false;
 
+    var wins = 0;
+
+    var losses = 0;
+
     var intervalId;
 
-    var questions = ["Q1", "Q2", "Q3"];
-
-    var answers = ["A1", "A2", "A3"];
-
-    function displayQuestion() {
-        $("#question").html("<h3>" + questions[count] + "</h3>");
+    var trivia = {
+        questions: [
+            {
+                question:"What are dogs most closely related to?",
+                choices: ["bears","pigs","cats","wolves"],
+                answer: "wolves"
+            },
+            {
+                question:"Akita, a dog, is a national treasure of which country?",
+                choices: ["Japan", "Spain", "Italy", "China"],
+                answer: "Japan"
+            },
+            {
+                question:"How many bones does a dog have in its spine?",
+                choices: ["12", "25", "31", "27"],
+                answer: "27"
+            },
+        ]
     }
 
-    function displayAnswer() {
+    function displayQuestion() {
 
-        if (correct === true) {
+        $("#question").html("<h3>" + trivia.questions[count].question + "</h3>");
+
+        for ( var i = 0; i < 4; i++) {
+
+            var list = trivia.questions[count].choices[i];
+            console.log(list);
+            $("#choices").append("<button class='list-group-item'>" + trivia.questions[count].choices[i] + "</button>");
+            
+        };
+
+        $(".list-group-item").on("click",function() {
+            userselect = $(this).val();
+            clearInterval(intervalId);
+            checkAnswer();
+        });
+
+    };
+
+    function checkAnswer() {
+
+        if (userselect == trivia.questions[count].answer) {
+            correct === true;
             $("#timeUp").html("<h2> Correct! </h2>");
+            wins++;
         }
         else {
+            correct === false;
             $("#timeUp").html("<h2> Nope! </h2>");
+            losses++;
         }
 
-        $("#question").html("<h3>" + answers[count] + "</h3>");
+        displayAnswer();
+    };
+
+    function displayAnswer() {
+        $("#question").html("<h3> Answer: " + trivia.questions[count].answer + "</h3>");
+        nextQuestion();
+    };
+
+    function nextQuestion() {
+        count++;
+
+        if (count === trivia.questions.length) {
+            stop();
+        }
+
+        setTimeout(reset, 3000);
+
+    };
+
+    function reset() {
+        $("#choices").html("");
+        $("#timeUp").html("");
+        number = 3;
+        intervalId = setInterval(decrement, 1000);
+        displayQuestion();
     }
 
     function start() {
         $("#startBtn").hide();
         intervalId = setInterval(decrement, 1000);
-        displayQuestion;
+        displayQuestion();
     }
 
     function stop() {
@@ -45,12 +109,10 @@ $(document).ready(function() {
         if (number === 0) {
             stop();
             $("#timeUp").html("<h2> Time Up !! </h2>");
-            displayAnswer;
+            displayAnswer();
         }
     }
 
     $("#startBtn").on("click", start);
-
-    $(".answerBtn").on("click", stop);
 
 });
